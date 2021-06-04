ed9ab897629c0903638afb7bc2f9e0271fb47699
PIYUSH NEGI <43876655+npiyush97@users.noreply.github.com>
2021-06-04 22:23:30 +0530

    Add files via upload

--- dayornight.js
+++ dayornight.js
@@ -1,28 +1,32 @@
-window.load = update()
+window.load = update();
 
-function update(){
-    let date = new Date().getHours()
-    let h1 = document.querySelector('h1')
-    console.log(date)
-    if(date >= 12){
-        h1.classList.remove('morning')
-        h1.classList.add('day')
-        h1.textContent = 'GOOD AFTERNOON'
-    }
-    if(date > 18){
-        h1.classList.remove('day')
-        h1.classList.add('night')
-        h1.textContent = 'GOOD NIGHT'
-    }
-    if(date >= 6){
-        h1.classList.remove('night')
-        h1.classList.add('morning')
-        h1.textContent = 'GOOD MORNING'
-    }
-    console.log('called')
+function update() {
+  let date = new Date().getHours();
+  let h1 = document.querySelector("h1");
+  console.log(date);
+  let morning = 6;
+  let noon = 12;
+  let night = 7;
+  if (date >= morning && date < noon) {
+    h1.classList.remove("night");
+    h1.classList.add("morning");
+    h1.textContent = "GOOD MORNING";
+  }
+  if (date >= noon && date < night) {
+    h1.classList.remove("morning");
+    h1.classList.add("day");
+    h1.textContent = "GOOD AFTERNOON";
+  }
+  if (date >= night || date < morning) {
+    h1.classList.remove("day");
+    h1.classList.add("night");
+    console.log(h1.classList);
+    h1.textContent = "GOOD NIGHT";
+  }
+  console.log("called");
 }
 
 setInterval(() => {
-    console.log('running')
-    update()
-}, 30*60*1000);
+  console.log("running");
+  update();
+}, 30 * 60 * 1000);
